55ebb3d3fd3fdfb466d5411c804b9bfdc77ddf17
mando8119 <armando.g.barron1@gmail.com>
2024-04-27 12:04:05 -0700

    initial upload

---
+++ ESC_SpeedTest_1/ESC_SpeedTest_1.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo steer;
+Servo esc; //ESC - Electronic Speed Controller | This is attached to the motor propelling the rover
+           // 1000 Microseconds = Full speed Backwards (Motor spins Counterclockwise?)
+           // 1500 microseconds = Neutral
+           // 2000 Microseconds = Full speed Forward (Motor spins Clockwise?)
+int Steer_Position = 0;
+
+void setup(){
+  Serial.begin(9600);
+  steer.attach(4);
+  esc.attach(5);
+
+  steer.write(93);
+  esc.writeMicroseconds(1500);
+  delay(2000);
+}
+
+void loop(){
+  
+  for(int pos = 1500 ; pos <= 1700; pos+=50){
+    esc.writeMicroseconds(pos);
+    Serial.println(pos);
+    delay(1500);
+  }
+  esc.writeMicroseconds(1500);
+  delay(100000);
+
+
+}
